555ad0900baf4c226dd283a6fc260e365a2d609e
MaBad1 <abadiemathieu13@gmail.com>
2022-05-24 18:50:17 +0200

    cinématique intro + musique

--- src/CutsceneIntro.js
+++ src/CutsceneIntro.js
@@ -7,6 +7,7 @@ class cutIntro extends Phaser.Scene {
 
         this.load.video('Intro', 'assets/VideoImgs/CutsceneIntro.mp4','loadeddata', false, true);
         this.load.image('Skip', 'assets/images/UI/Skip.png');
+        this.load.audio('MainTheme', 'assets/sounds/LevelMusic.mp3');
 
     }
 
@@ -15,6 +16,8 @@ class cutIntro extends Phaser.Scene {
         this.video = this.add.video(1000, 400, 'Intro');
         this.video.setScale(0.7, 0.7);
         this.video.play();
+
+        this.mainTheme = this.sound.add('MainTheme',{volume: 0.3}).play();
 
         this.buttonSkipSprite = this.add.image(770, 800, 'Skip')
             .setOrigin(0, 0)
